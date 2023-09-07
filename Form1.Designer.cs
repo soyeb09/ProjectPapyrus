9f09e9819dc10bb8acefa0c1673cccabc9a11b95
Md. Mofizur Rahman <91398637+soyeb09@users.noreply.github.com>
2023-09-08 03:47:03 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ProjectProtectedPapyrus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.siticoneRoundedButton1 = new Siticone.UI.WinForms.SiticoneRoundedButton();
+            this.siticoneRoundedTextBox1 = new Siticone.UI.WinForms.SiticoneRoundedTextBox();
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneLabel2 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.SuspendLayout();
+            // 
+            // siticoneRoundedButton1
+            // 
+            this.siticoneRoundedButton1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneRoundedButton1.BorderThickness = 2;
+            this.siticoneRoundedButton1.CheckedState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.CustomImages.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.siticoneRoundedButton1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneRoundedButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedButton1.HoveredState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Location = new System.Drawing.Point(33, 155);
+            this.siticoneRoundedButton1.Name = "siticoneRoundedButton1";
+            this.siticoneRoundedButton1.ShadowDecoration.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Size = new System.Drawing.Size(200, 32);
+            this.siticoneRoundedButton1.TabIndex = 0;
+            this.siticoneRoundedButton1.Text = "Login";
+            this.siticoneRoundedButton1.Click += new System.EventHandler(this.siticoneRoundedButton1_Click);
+            // 
+            // siticoneRoundedTextBox1
+            // 
+            this.siticoneRoundedTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneRoundedTextBox1.BorderColor = System.Drawing.Color.Black;
+            this.siticoneRoundedTextBox1.BorderThickness = 2;
+            this.siticoneRoundedTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneRoundedTextBox1.DefaultText = "";
+            this.siticoneRoundedTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneRoundedTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneRoundedTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneRoundedTextBox1.DisabledState.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneRoundedTextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.siticoneRoundedTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneRoundedTextBox1.FocusedState.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedTextBox1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneRoundedTextBox1.HoveredState.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.Location = new System.Drawing.Point(33, 120);
+            this.siticoneRoundedTextBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.siticoneRoundedTextBox1.Name = "siticoneRoundedTextBox1";
+            this.siticoneRoundedTextBox1.PasswordChar = '\0';
+            this.siticoneRoundedTextBox1.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.siticoneRoundedTextBox1.PlaceholderText = "";
+            this.siticoneRoundedTextBox1.SelectedText = "";
+            this.siticoneRoundedTextBox1.ShadowDecoration.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.Size = new System.Drawing.Size(200, 32);
+            this.siticoneRoundedTextBox1.TabIndex = 1;
+            this.siticoneRoundedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Black;
+            this.siticoneLabel1.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel1.IsSelectionEnabled = false;
+            this.siticoneLabel1.Location = new System.Drawing.Point(240, 1);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(10, 15);
+            this.siticoneLabel1.TabIndex = 3;
+            this.siticoneLabel1.Text = "X";
+            this.siticoneLabel1.Click += new System.EventHandler(this.siticoneLabel1_Click);
+            // 
+            // siticoneLabel2
+            // 
+            this.siticoneLabel2.BackColor = System.Drawing.Color.Black;
+            this.siticoneLabel2.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel2.IsSelectionEnabled = false;
+            this.siticoneLabel2.Location = new System.Drawing.Point(224, 1);
+            this.siticoneLabel2.Name = "siticoneLabel2";
+            this.siticoneLabel2.Size = new System.Drawing.Size(9, 15);
+            this.siticoneLabel2.TabIndex = 4;
+            this.siticoneLabel2.Text = "—";
+            this.siticoneLabel2.Click += new System.EventHandler(this.siticoneLabel2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(260, 253);
+            this.Controls.Add(this.siticoneLabel2);
+            this.Controls.Add(this.siticoneLabel1);
+            this.Controls.Add(this.siticoneRoundedTextBox1);
+            this.Controls.Add(this.siticoneRoundedButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneRoundedButton siticoneRoundedButton1;
+        private Siticone.UI.WinForms.SiticoneRoundedTextBox siticoneRoundedTextBox1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectProtectedPapyrus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneRoundedButton1_Click(object sender, EventArgs e)
+        {
+            if(siticoneRoundedTextBox1.Text == "Papyrus")
+            {
+                this.Hide();
+                var Dashboard = new Dashboard();
+                Dashboard.Show();
+            }
+            else
+            {
+                siticoneRoundedButton1.FillColor = Color.Red;
+                siticoneRoundedButton1.Text = "Login Again";
+            }
+        }
+
+        private void siticoneLabel1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void siticoneLabel2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
